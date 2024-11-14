00c07687f0fd9dc8588c43d6609b9a4a2befd8d3
KhoiTranAnh <trananhkhoitv@gmail.com>
2024-11-14 09:49:00 +0700

    [FEAT] Create 'create user account api'

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotevn from 'dotenv'
-import {connectDB} from './config/db.js'
-import User from './models/User.model.js';
+import { connectDB } from './config/db.js'
+import userRoute from './routes/user.route.js'
 
 dotevn.config();
 
@@ -15,34 +15,7 @@ app.get('/', (req, res) => {
   // res.send('Hello World! test nodemon')
 })
 
-// SAMPLE CREATE USER API, CAN BE DELETED
-app.post('/api/users', async (req, res) => {
-  const user = req.body;
-
-  if (!user.email || !user.password) {
-    return res.status(400).json({
-      success: false,
-      message: "Please fully provide email and password"
-    })
-  }
-
-  const newUser = new User(user);
-
-  try {
-    await newUser.save();
-    res.status(201).json({
-      success: true,
-      data: newUser
-    })
-  } catch (error) {
-    console.error("Error in CreateUser: ", error.message);
-    res.status(500).json({
-      success: false,
-      message: "Server Error"
-    })
-  }
-})
-// ##########################################################3
+app.use("/api/users", userRoute)
 
 app.listen(port, () => {
   connectDB();
