2d054c618d0fd2932f1d468f549ed01087940659
KhoiTranAnh <trananhkhoitv@gmail.com>
2024-12-29 21:14:41 +0700

    feat: view orderDetail

--- admin_page/src/components/UserInfo/UserInfo.jsx
+++ admin_page/src/components/UserInfo/UserInfo.jsx
@@ -1,23 +1,22 @@
-import { Input } from '@mui/base/Input';
-
-function UserInfo() {
+/* eslint-disable react/prop-types */
+function UserInfo({email, firstName, lastName, phone, shippingAddress, note}) {
   return (
     <div className='flex flex-col flex-grow w-1/3 h-fit border border-[#93C8ED] rounded-lg p-4 gap-3'>
         <p className="text-lg font-semibold">Thông tin nhận hàng</p>
         
         <div className='flex flex-col'>
             <p className='text-sm text-gray-700'>Email</p>
-            <input type="text" disabled value={"khoi.trananh2004@hcmut.edu.vn"} className='w-full py-2 px-3 rounded-md border border-[#B6DAF3]'/>
+            <input type="text" disabled value={email} className='w-full py-2 px-3 rounded-md border border-[#B6DAF3]'/>
         </div>
 
         <div className='flex flex-col'>
             <p className='text-sm text-gray-700'>Tên người nhận</p>
-            <input type="text" disabled value={"Trần Anh Khôi"} className='w-full py-2 px-3 rounded-md border border-[#B6DAF3]'/>
+            <input type="text" disabled value={`${firstName} ${lastName}`} className='w-full py-2 px-3 rounded-md border border-[#B6DAF3]'/>
         </div>
 
         <div className='flex flex-col'>
             <p className='text-sm text-gray-700'>Số điện thoại</p>
-            <input type="text" disabled value={"0123456789"} className='w-full py-2 px-3 rounded-md border border-[#B6DAF3]'/>
+            <input type="text" disabled value={phone} className='w-full py-2 px-3 rounded-md border border-[#B6DAF3]'/>
         </div>
 
         <div className='flex flex-col'>
@@ -42,7 +41,7 @@ function UserInfo() {
 
         <div className='flex flex-col'>
             <p className='text-sm text-gray-700'>Ghi chú</p>
-            <input type="text" disabled value={"Sốp muốn ship đi đâu thì ship"} className='w-full py-2 px-3 rounded-md border border-[#B6DAF3]'/>
+            <input type="text" disabled value={note} className='w-full py-2 px-3 rounded-md border border-[#B6DAF3]'/>
         </div>
         
     </div>
--- admin_page/src/pages/OrderDetail.jsx
+++ admin_page/src/pages/OrderDetail.jsx
@@ -1,22 +1,120 @@
 import { useParams } from 'react-router-dom'
 import OrderDetailInfo from '../components/OrderDetailInfo/OrderDetailInfo'
 import UserInfo from '../components/UserInfo/UserInfo'
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-import InputLabel from '@mui/material/InputLabel';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
 
 function OrderDetail() {
-    const [status, setStatus] = useState("Confirmed")
+    const navigate = useNavigate();
+    const url = 'http://localhost:8000'
+    const [status, setStatus] = useState("")
 
     const {orderId} = useParams()
+
+    const [order, setOrder] = useState({});
+    const [user, setUser] = useState({});
+
+    const [newStatus, setNewStatus] = useState("");
+
+    const [disableButton, setDisableButton] = useState([true]);
+
+    const [options, setOptions] = useState([
+        <option key={0} value="confirmed">Đã xác nhận</option>,
+        <option key={1} value="exported">Đã xuất hàng</option>,
+        <option key={2} value="delivering">Đang giao hàng</option>,
+        <option key={3} value="delivered">Đã giao hàng</option>]);
+
+    useEffect(() => {
+        if (status === newStatus) setDisableButton(true);
+        else setDisableButton(false);
+    }, [newStatus])
+
+    useEffect(() => {
+        const fetchOrder = async (orderId) => {
+            try {
+                const respose = await axios.get(`${url}/api/order/${orderId}`);
+                // console.log(respose.data.order);
+                setOrder((order) => ({...order, ...respose.data.order}));
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        
+        fetchOrder(orderId);
+    }, [orderId])
+
+    useEffect(() => {
+        const getOptions = (status) => {
+            if (status === 'confirmed') return (
+                [<option key={0} value="confirmed" selected>Đã xác nhận</option>,
+                    <option key={1} value="exported">Đã xuất hàng</option>,
+                    <option key={2} value="delivering">Đang giao hàng</option>,
+                    <option key={3} value="delivered">Đã giao hàng</option>]
+                );
+            else if (status === 'exported') return (
+                [<option key={0} value="confirmed">Đã xác nhận</option>,
+                    <option key={1} value="exported" selected>Đã xuất hàng</option>,
+                    <option key={2} value="delivering">Đang giao hàng</option>,
+                    <option key={3} value="delivered">Đã giao hàng</option>]
+            );
+            else if (status === 'delivering') return (
+                [<option key={0} value="confirmed" >Đã xác nhận</option>,
+                    <option key={1} value="exported">Đã xuất hàng</option>,
+                    <option key={2} value="delivering" selected>Đang giao hàng</option>,
+                    <option key={3} value="delivered">Đã giao hàng</option>]
+            );
+            else if (status === 'delivered') return (
+                [<option key={0} value="confirmed" >Đã xác nhận</option>,
+                    <option key={1} value="exported">Đã xuất hàng</option>,
+                    <option key={2} value="delivering">Đang giao hàng</option>,
+                    <option key={3} value="delivered" selected>Đã giao hàng</option>]
+            )
+        }
+
+        const fetchUser = async (userId) => {
+            try {
+                const respose = await axios.get(`${url}/api/user/${userId}`);
+                // console.log("user:", respose.data);
+                setUser((user) => ({...user, ...respose.data}));
+            } catch (error) {
+                console.error(error)
+            }
+        }
+
+        fetchUser(order.user);
+        setStatus(order.status)
+        setOptions(getOptions(order.status));
+        
+    }, [order])
+
+    const updateOrderStatus = async () => {
+        try {
+            const response = await axios.put(`http://localhost:8000/api/order/${orderId}`, {
+                status: newStatus
+            })
+            if (response.status) {
+                alert("Cập nhật tình trạng đơn hàng thành công");
+                window.location.reload();
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const debug = () => {
+        console.log(order);
+        console.log(options);
+        console.log(disableButton);
+    }
+
     return (
         <div className='w-screen h-screen'>
             <div className='flex flex-col m-auto bg-[#E8F3FB] w-3/4 h-fit items-center gap-5 py-3 px-10 border border-[#93C8ED] rounded-md mb-5'>
-                <p className='text-xl font-bold'>{`Chi tiết đơn hàng ${orderId}`}</p>
+                <p className='text-xl font-bold'>{`Chi tiết đơn hàng #${orderId}`}</p>
                 <div className='flex flex-row justify-between w-full h-full gap-5'>
-                    <OrderDetailInfo/>
-                    <UserInfo/>
+                    <OrderDetailInfo order={order}/>
+                    <UserInfo email={user.email} firstName={user.firstName} lastName={user.lastName} note={order.note} shippingAddress={order.shippingAddress} phone={user.phone}/>
                 </div>
                 <div className='flex flex-row w-full h-fit gap-5 item items-end'>
                     <div className='flex flex-col flex-grow w-2/3 h-fit  border rounded-lg border-[#93C8ED] px-4 py-2 gap-2'>
@@ -30,26 +128,18 @@ function OrderDetail() {
                         </div>
                     </div>
                     
-                    <div className='w-1/3'>
-                        <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                            Tình trạng đơn hàng
-                        </InputLabel>
-                        <Select
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            value={status}
-                            label="Tình trạng đơn hàng"
-                            sx={{width: "100%"}}
-                            onChange={(event) => {setStatus(event.target.value)}} 
-                        >
-                            <MenuItem value={"Confirmed"}>Đã xác nhận</MenuItem>
-                            <MenuItem value={"Exported"}>Đã xuất hàng</MenuItem>
-                            <MenuItem value={'Delivering'}>Đang giao hàng</MenuItem>
-                            <MenuItem value={'Delivered'}>Đã giao hàng</MenuItem>
-                        </Select>
+                    <div className='flex flex-row w-1/3 gap-2 justify-between'>
+                        <select className='w-2/3' onChange={(event) => {setNewStatus(event.target.value)}}>
+                            {options}
+                        </select>
+                        {disableButton==true?
+                        <button disabled className='px-3 rounded-md bg-blue-600 text-white font-bold border border-[#93C8ED] hover:bg-blue-800 disabled:bg-gray-500 text-center'>Cập nhật</button> 
+                        :
+                        <button onClick={updateOrderStatus} className='px-3 rounded-md bg-blue-600 text-white font-bold border border-[#93C8ED] hover:bg-blue-800 disabled:bg-gray-500 text-center'>Cập nhật</button>}
                     </div>
                 </div>
             </div>
+            <button onClick={debug} className='hidden border p-4 disabled:bg-gray-600'>debug</button>
         </div>
     )
 }
